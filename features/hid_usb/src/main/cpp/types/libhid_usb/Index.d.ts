d059135cd26188597ef60baebe28f421a4cec419
2847714478 <2847714478@qq.com>
2025-09-09 18:12:30 +0800

    分包+usb端口通信+热插拔检测+登录检测,未检测到重复检测+实时监听——————语音通信1b暂缺

--- features/hid_usb/src/main/cpp/types/libhid_usb/Index.d.ts
+++
@@ -1,9 +0,0 @@
-export const initddk: () => number;
-export const open: (deviceId: number, interfaceIndex: number) => number;
-export const sendReport: (reportType: number, data: ArrayBuffer, length: number) => number;
-export const read: (bufSize: number, timeout?: number) => {
-  result: number;
-  bytesRead: number;
-  data: ArrayBuffer
-};
-export const closeHidDevice: () => number;
